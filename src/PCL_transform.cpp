3e40712f659c3b262b3a410bae0a9fa995545170
Vladimir Li <vladde1991@gmail.com>
2016-08-13 11:04:03 +0200

    removed includes, added comments, changed names

--- src/PCL_transform.cpp
+++ src/PCL_transform.cpp
@@ -1,61 +1,37 @@
-//#include "PCL_includes.h"
-#include <ros/ros.h>
-#include <ros/console.h>
-#include <signal.h>
-#include <iostream>
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/filters/radius_outlier_removal.h>
-#include <pcl/filters/conditional_removal.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/kdtree/kdtree.h>
-#include <tf/transform_listener.h>
-#include <pcl_ros/transforms.h>
+#include "PCL_transform.h"
+
 ros::Publisher tf_pub;
 tf::TransformListener *tf_listener;
 
 void callback(const sensor_msgs::PointCloud2ConstPtr& input)
 {
-  pcl::PCLPointCloud2 pcl_pc2;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_out(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl_conversions::toPCL(*input,pcl_pc2);
+ // Converting from PointCloud2 msg to pcl::PointCloud
+ pcl::PCLPointCloud2 pcl_pc2;
+ pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_out(new pcl::PointCloud<pcl::PointXYZ>);
+ pcl_conversions::toPCL(*input,pcl_pc2);
+ pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_in(new pcl::PointCloud<pcl::PointXYZ>);
+ pcl::fromPCLPointCloud2(pcl_pc2,*pcl_in);
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_in(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::fromPCLPointCloud2(pcl_pc2,*pcl_in);
+ // waiting for the transform and transform point cloud from its frame to the /camera_link frame
+ // You might need to change camera_link frame to what ever frame you would like your
+ // pointcloud to be transformed to...
+ tf_listener->waitForTransform("/camera_link", (*pcl_in).header.frame_id, input->header.stamp, ros::Duration(5.0));
+ pcl_ros::transformPointCloud("/camera_link", *pcl_in, *pcl_out, *tf_listener);
 
-  ros::Time now = input->header.stamp;
-  tf_listener->waitForTransform("/camera_link", (*pcl_in).header.frame_id, now, ros::Duration(5.0));
-  pcl_ros::transformPointCloud("/camera_link", *pcl_in, *pcl_out, *tf_listener);
-
-
-  sensor_msgs::PointCloud2 output;
-  pcl::toROSMsg (*pcl_out, output);
-  tf_pub.publish(output);
+ sensor_msgs::PointCloud2 output;
+ pcl::toROSMsg (*pcl_out, output);
+ tf_pub.publish(output);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "PCL_transform");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/camera/depth/filtered_points", 1, callback);
-  tf_pub = nh.advertise<sensor_msgs::PointCloud2> ("/camera/depth/cloud_transformed", 1);
+ ros::init(argc, argv, "PCL_transform");
+ ros::NodeHandle nh;
+ ros::Subscriber sub = nh.subscribe("/camera/depth/filtered_points", 1, callback);
+ tf_pub = nh.advertise<sensor_msgs::PointCloud2> ("/camera/depth/cloud_transformed", 1);
 
-  tf_listener    = new tf::TransformListener();
+ tf_listener    = new tf::TransformListener();
 
-  ros::spin();
-  //delete tf_listener;
-  //return 0;
+ ros::spin();
+ return 0;
 }
